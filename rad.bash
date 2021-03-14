89cb9311bc717396827867ee5612421d89f8b46e
maybe-sybr <58414429+maybe-sybr@users.noreply.github.com>
2021-03-15 10:12:09 +1100

    fix: Make fake-`forall` use absolute repo paths
    This ensures we can run rad command in subdirectories of the worktree.

--- rad.bash
+++ rad.bash
@@ -108,7 +108,7 @@ function _repo_wrap_inner () {
     # We can't use `forall` to run interactive commands since v2.13 (fbab606
     # specifically) and it's easier to just loop over the paths anyway since it
     # avoids allowing `repo` to choose some level of parallelism > 1.
-    IFS=$'\n' declare -ar REPO_PATHS=( $(repo list --path-only) )
+    IFS=$'\n' declare -ar REPO_PATHS=( $(repo list --path-only --fullpath) )
     for repo_path in "${REPO_PATHS[@]}"; do
         (
             # `repo` provides lots of these so we may need to add more later
